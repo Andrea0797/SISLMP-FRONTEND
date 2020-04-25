9a578af280d4fc6280e374a25e00d851882a85dc
Gianina Andrea Ramirez Espinoza <gramiree@everis.com>
2020-04-25 07:40:57 -0500

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,17 @@ export default class Home extends Component {
             <Layout { ...this.props }>
                 <div>
                     <br />
-                    <h5>BIENVENIDO AL SISTEMA DE CONSULTAS DE MEDICAMENTOS</h5>
-                    <img
+                    <h5 style={{textAlign:"center"}}>BIENVENIDO AL SISTEMA DE CONSULTAS DE MEDICAMENTOS</h5>
+                    
+                                <div className="border rounded p-3 mt-4" style={{ height: 420}}>
+                                <img
                                     src="http://www.digemid.minsa.gob.pe/UpLoad/UpLoaded/JPG/Noticias/NotaPrensa/Banner/2020/03/B07_2020_03-10.JPG"
                                     alt="img"
                                     style={{
                                         width: "100%", objectFit: "contain", height: "100%"
                                     }}
                                 />
+                            </div>
                 </div>
             </Layout>
         )
